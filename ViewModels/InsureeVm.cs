e900951fbc62c95e7d43cfb8f1a6e68658ea5e1e
Clare Shin <haeyin.shin@gmail.com>
2020-04-17 03:17:52 -0700

    tests

--- ViewModels/InsureeVm.cs
+++ ViewModels/InsureeVm.cs
@@ -7,5 +7,15 @@ namespace Assignment_30b.ViewModels
 {
     public class InsureeVm
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime Birthday { get; set; }
+        public int CarYear { get; set; }
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public bool DUI { get; set; }
+        public int SpeedingTickets { get; set; }
+        public bool CoverageType { get; set; }
     }
 }
